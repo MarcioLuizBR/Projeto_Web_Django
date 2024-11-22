f31ce4d7bb2b508140eed98f7e318fc582d2cd30
Marcio Luiz dos Santos Pereira <133532252+MarcioLuizBR@users.noreply.github.com>
2024-11-22 00:09:06 -0300

    Usuario personalizado

--- filme/admin.py
+++ filme/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Filme, Episodio
+from .models import Filme, Episodio, Usuario
+from django.contrib.auth.admin import UserAdmin
+
+
+# foi necessário esta abordagem abaixo para aparecer o campo personalizado filmes_visto
+campos = list(UserAdmin.fieldsets)
+campos.append(
+    ("Histórico", {'fields': ('filmes_vistos',)})
+)
+UserAdmin.fieldsets = tuple(campos)
+
 
 # Register your models here.
 admin.site.register(Filme)
 admin.site.register(Episodio)
+admin.site.register(Usuario, UserAdmin)
