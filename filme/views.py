01802902b38851e366d1525a227e15fb177e0cb3
Marcio Luiz dos Santos Pereira <133532252+MarcioLuizBR@users.noreply.github.com>
2024-11-23 15:03:24 -0300

    editar perfil mudar senha

--- filme/views.py
+++ filme/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect, reverse
-from .models import Filme
-from .forms import CriarContaForm
-from django.views.generic import TemplateView, ListView, DetailView, FormView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomepage
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = "homepage.html"
+    form_class = FormHomepage
     
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('filme:homefilmes')
         else:
             return super().get(request, *args, **kwargs) # redireciona para a homepage
+    
+    def get_success_url(self):
+        email = self.request.POST.get('email')
+        usuario = Usuario.objects.filter(email=email)
+        if usuario:
+            return reverse('filme:login')
+        else:
+            return reverse('filme:criarconta')
 
 
 class Homefilmes(LoginRequiredMixin, ListView):
@@ -52,9 +61,20 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
             return object_list
         else:
             return None
-        
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+
+class Paginaperfil(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'editarperfil.html'
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+    def test_func(self):
+        usuario = self.get_object()
+        return self.request.user == usuario
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
+           
+
 
 class Criarconta(FormView):
     template_name = 'criarconta.html'
