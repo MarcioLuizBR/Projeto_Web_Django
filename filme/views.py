6256991897a23dff94aceb77015be414daa71e47
Marcio Luiz dos Santos Pereira <133532252+MarcioLuizBR@users.noreply.github.com>
2024-11-22 22:41:28 -0300

    criar conta usuario

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Filme
-from django.views.generic import TemplateView, ListView, DetailView
+from .forms import CriarContaForm
+from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -55,8 +56,17 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
 class Paginaperfil(LoginRequiredMixin, TemplateView):
     template_name = 'editarperfil.html'
 
-class Criarconta(TemplateView):
+class Criarconta(FormView):
     template_name = 'criarconta.html'
+    form_class = CriarContaForm
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    
+    def get_success_url(self):
+        return reverse('filme:login')
 
 
 
